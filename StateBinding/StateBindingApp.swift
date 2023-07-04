0ac9598fee73048666ad6cfcb439c000eb7c2c0d
brunocamps <bruno.mcamps@outlook.com>
2023-07-04 09:30:30 -0300

    Initial Commit

---
+++ StateBinding/StateBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateBindingApp.swift
+//  StateBinding
+//
+//  Created by Bruno Campos on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct StateBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
